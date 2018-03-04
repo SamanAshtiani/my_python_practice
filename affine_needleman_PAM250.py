c768fcdaa8918a49271333dbb7fc660461dea375
SamanAshtiani <samanh.ashtiani@gmail.com>
2018-03-04 23:12:33 +0100

    add x and y calculations for e

---
+++ affine_needleman_PAM250.py
@@ -0,0 +1,107 @@
+#!/usr/bin/evn python3
+
+import numpy as np
+
+
+pam_dict = {
+'A': {'A': 2,  'C': -2, 'D':  0, 'E': 0, 'F': -3, 'G':  1, 'H': -1, 'I': -1, 'K': -1, 'L': -2, 'M': -1, 'N':  0, 'P':  1, 'Q':  0, 'R': -2, 'S':  1, 'T':  1, 'V':  0, 'W': -6, 'Y': -3},
+'C': {'A': -2, 'C': 12, 'D': -5, 'E':-5, 'F': -4, 'G': -3, 'H': -3, 'I': -2, 'K': -5, 'L': -6, 'M': -5, 'N': -4, 'P': -3, 'Q': -5, 'R': -4, 'S':  0, 'T': -2, 'V': -2, 'W': -8, 'Y':  0},
+'D': {'A': 0,  'C': -5, 'D':  4, 'E': 3, 'F': -6, 'G':  1, 'H':  1, 'I': -2, 'K':  0, 'L': -4, 'M': -3, 'N':  2, 'P': -1, 'Q':  2, 'R': -1, 'S':  0, 'T':  0, 'V': -2, 'W': -7, 'Y': -4},
+'E': {'A': 0,  'C': -5, 'D':  3, 'E': 4, 'F': -5, 'G':  0, 'H':  1, 'I': -2, 'K':  0, 'L': -3, 'M': -2, 'N':  1, 'P': -1, 'Q':  2, 'R': -1, 'S':  0, 'T':  0, 'V': -2, 'W': -7, 'Y': -4},
+'F': {'A': -3, 'C': -4, 'D': -6, 'E':-5, 'F':  9, 'G': -5, 'H': -2, 'I':  1, 'K': -5, 'L':  2, 'M':  0, 'N': -3, 'P': -5, 'Q': -5, 'R': -4, 'S': -3, 'T': -3, 'V': -1, 'W':  0, 'Y':  7},
+'G': {'A': 1,  'C': -3, 'D':  1, 'E': 0, 'F': -5, 'G':  5, 'H': -2, 'I': -3, 'K': -2, 'L': -4, 'M': -3, 'N':  0, 'P':  0, 'Q': -1, 'R': -3, 'S':  1, 'T':  0, 'V': -1, 'W': -7, 'Y': -5},
+'H': {'A': -1, 'C': -3, 'D':  1, 'E': 1, 'F': -2, 'G': -2, 'H':  6, 'I': -2, 'K':  0, 'L': -2, 'M': -2, 'N':  2, 'P':  0, 'Q':  3, 'R':  2, 'S': -1, 'T': -1, 'V': -2, 'W': -3, 'Y':  0},
+'I': {'A': -1, 'C': -2, 'D': -2, 'E':-2, 'F':  1, 'G': -3, 'H': -2, 'I':  5, 'K': -2, 'L':  2, 'M':  2, 'N': -2, 'P': -2, 'Q': -2, 'R': -2, 'S': -1, 'T':  0, 'V':  4, 'W': -5, 'Y': -1},
+'K': {'A': -1, 'C': -5, 'D':  0, 'E': 0, 'F': -5, 'G': -2, 'H':  0, 'I': -2, 'K':  5, 'L': -3, 'M':  0, 'N':  1, 'P': -1, 'Q':  1, 'R':  3, 'S':  0, 'T':  0, 'V': -2, 'W': -3, 'Y': -4},
+'L': {'A': -2, 'C': -6, 'D': -4, 'E':-3, 'F':  2, 'G': -4, 'H': -2, 'I':  2, 'K': -3, 'L':  6, 'M':  4, 'N': -3, 'P': -3, 'Q': -2, 'R': -3, 'S': -3, 'T': -2, 'V':  2, 'W': -2, 'Y': -1},
+'M': {'A': -1, 'C': -5, 'D': -3, 'E':-2, 'F':  0, 'G': -3, 'H': -2, 'I':  2, 'K':  0, 'L':  4, 'M':  6, 'N': -2, 'P': -2, 'Q': -1, 'R':  0, 'S': -2, 'T': -1, 'V':  2, 'W': -4, 'Y': -2},
+'N': {'A': 0,  'C': -4, 'D':  2, 'E': 1, 'F': -3, 'G':  0, 'H':  2, 'I': -2, 'K':  1, 'L': -3, 'M': -2, 'N':  2, 'P':  0, 'Q':  1, 'R':  0, 'S':  1, 'T':  0, 'V': -2, 'W': -4, 'Y': -2},
+'P': {'A': 1,  'C': -3, 'D': -1, 'E':-1, 'F': -5, 'G':  0, 'H':  0, 'I': -2, 'K': -1, 'L': -3, 'M': -2, 'N':  0, 'P':  6, 'Q':  0, 'R':  0, 'S':  1, 'T':  0, 'V': -1, 'W': -6, 'Y': -5},
+'Q': {'A': 0,  'C': -5, 'D':  2, 'E': 2, 'F': -5, 'G': -1, 'H':  3, 'I': -2, 'K':  1, 'L': -2, 'M': -1, 'N':  1, 'P':  0, 'Q':  4, 'R':  1, 'S': -1, 'T': -1, 'V': -2, 'W': -5, 'Y': -4},
+'R': {'A': -2, 'C': -4, 'D': -1, 'E':-1, 'F': -4, 'G': -3, 'H':  2, 'I': -2, 'K':  3, 'L': -3, 'M':  0, 'N':  0, 'P':  0, 'Q':  1, 'R':  6, 'S':  0, 'T': -1, 'V': -2, 'W':  2, 'Y': -4},
+'S': {'A': 1,  'C':  0, 'D':  0, 'E': 0, 'F': -3, 'G':  1, 'H': -1, 'I': -1, 'K':  0, 'L': -3, 'M': -2, 'N':  1, 'P':  1, 'Q': -1, 'R':  0, 'S':  2, 'T':  1, 'V': -1, 'W': -2, 'Y': -3},
+'T': {'A': 1,  'C': -2, 'D':  0, 'E': 0, 'F': -3, 'G':  0, 'H': -1, 'I':  0, 'K':  0, 'L': -2, 'M': -1, 'N':  0, 'P':  0, 'Q': -1, 'R': -1, 'S':  1, 'T':  3, 'V':  0, 'W': -5, 'Y': -3},
+'V': {'A': 0,  'C': -2, 'D': -2, 'E':-2, 'F': -1, 'G': -1, 'H': -2, 'I':  4, 'K': -2, 'L':  2, 'M':  2, 'N': -2, 'P': -1, 'Q': -2, 'R': -2, 'S': -1, 'T':  0, 'V':  4, 'W': -6, 'Y': -2},
+'W': {'A': -6, 'C': -8, 'D': -7, 'E':-7, 'F':  0, 'G': -7, 'H': -3, 'I': -5, 'K': -3, 'L': -2, 'M': -4, 'N': -4, 'P': -6, 'Q': -5, 'R':  2, 'S': -2, 'T': -5, 'V': -6, 'W': 17, 'Y':  0},
+'Y': {'A': -3, 'C':  0, 'D': -4, 'E':-4, 'F':  7, 'G': -5, 'H':  0, 'I': -1, 'K': -4, 'L': -1, 'M': -2, 'N': -2, 'P': -5, 'Q': -4, 'R': -4, 'S': -3, 'T': -3, 'V': -2, 'W':  0, 'Y': 10}}
+
+
+def get_score(a, b):
+    return pam_dict[a][b]
+
+
+def align(a, b, g=-5):
+    # match = 1
+    # mismatch = -1
+    n = len(a)
+    m = len(b)
+    ali_matrix = np.zeros((n+1, m+1))
+    # print(ali_matrix)
+    for i in range(0, n+1):
+        ali_matrix[i, 0] = i*g
+    # print(ali_matrix)
+    for j in range(0, m+1):
+        ali_matrix[0, j] = j*g
+    # print(ali_matrix)
+    #y_matrix = np.zeros((n+1, m+1))
+    #x_matrix = np.zeros((n+1, m+1))
+
+    origin_of_direction_score_dict = {}
+    for i in range(n):
+        # print("the {}th element of seq a : {}".format(i+1, a[i]))
+        for j in range(m):
+            e_y =
+            e_x =
+            ali_matrix[i+1,j+1] = max(ali_matrix[i, j+1] + g, ali_matrix[i, j+1] + e_y , ali_matrix[i+1, j] + g, ali_matrix[i+1, j] +e_x , ali_matrix[i, j] + get_score(a[i], b[j]),)
+            origin_of_direction_score_dict[(i+1, j+1)] = {"v": ali_matrix[i, j+1] + g,
+                                      "h": ali_matrix[i+1, j] + g,
+                                      "d": ali_matrix[i, j]+ get_score(a[i], b[j])}
+    print("down-rightmost element index-->max of n and m plus 1 ie n=4+1, m=5+1: ", ali_matrix[4, 5])
+    print("diagonal value+s from which the down-rightmost element was assigned its value: ", origin_of_direction_score_dict[(4, 5)]["d"])
+    print(ali_matrix)
+
+
+    aligned_a = ""
+    aligned_b = ""
+    i = n
+    j = m
+    # on the matrix last element: i, j on the sequences last elements are i-1 and j-1
+    print(a[i-1], b[j-1])
+    print(ali_matrix[i,j])
+    print(ali_matrix[0, 0])
+    print("diagonally one pair before the last pairs:", a[i-2], b[j-2])
+    while i>0 or j>0:
+        # print("downrightmost element after assigning the lengths of seqs to i&j: ", ali_matrix[n, m])
+        if i>0 and j>0 and ali_matrix[i, j] == origin_of_direction_score_dict[(i, j)]["d"]:
+            # print("vertical origin of direction score with new i and j: ", origin_of_direction_score_dict[i,j]["h"])
+            # print("previous (diagonal) elements from a is {} and from b is {}".format(a[i-2], b[j-2]))
+            aligned_a = str(a[i-1]) + aligned_a # i&j on seq are already 1 less than
+            aligned_b = str(b[j-1]) + aligned_b
+            # print("b[j]=", str(b[j - 1]))
+            i -= 1
+            j -= 1
+        elif i>0 and ali_matrix[i, j] == origin_of_direction_score_dict[(i, j)]["v"]:
+            aligned_a = str(a[i-1]) + aligned_a
+            aligned_b = "-" + aligned_b
+            i -= 1
+        elif j>0 and ali_matrix[i, j] == origin_of_direction_score_dict[(i, j)]["h"]:
+            aligned_a = "-" + aligned_a
+            aligned_b = str(b[j-1]) + aligned_b
+            j -= 1
+        # print("b[j]=", str(b[j - 1]))
+
+    print(aligned_a)
+    print(aligned_b)
+
+
+
+
+if __name__ == '__main__':
+    align('GACCTACG','GTGMRKLTACA')
+
+
+
+
+
+
+
