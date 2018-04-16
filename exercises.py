a57f04a5ed4970945c22912302857c38b0d076f7
samanashtiani <saman.hosseini-ashtiani@dbb.su.se>
2018-04-16 17:42:51 +0200

    Dictionary-based version of dna2aa

--- exercises.py
+++ exercises.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python3
 
+with open("all.txt", 'r') as fh:
+    entries = {}
+    for l in fh:
+        # print(l.strip())
+        l = l.strip()
+        if l.startswith(">"):
+            header = l[1:]
+            temp_str = ""
+            entries[header] = temp_str
+        else:
+            temp_str += l
+            entries[header] = temp_str
+for k, v in entries.items():
+    print(k, v)
 
-# for k in set(lab_a).intersection(set(lab_b)):    #lab_a here is equivalent to lab_a.keys()
-#     lab_a[k], lab_b[k]
 
 
-# result = []
-# list_a = [1,2,3,4,6,5]
-# for i in list_a:
-#     if i%2 == 1:
-#         result.append(i**2)
-# print(result)
-#
-# result = [i**2 for i in list_a if i%2 == 1]
-# print("comprehension result ", result)
-#
-# result2 = []
-# for i in list_a:
-#     if i%2 == 0:
-#         result2.append(i)
-#     else:
-#         result2.append(i**2)
-# print(result2)
-#
-# result2 = [i if i%2 == 0 else i**2 for i in list_a]
-# print("comprehension result2: ", result2)
-
-dict = {"sam": 2, "Zahra": 3, "diana": 6}
-print(dict)
