611ed753951a12969ff6a6c35259535e73edd7da
samanashtiani <saman.hosseini-ashtiani@dbb.su.se>
2018-07-26 16:41:40 +0200

    minor improvement and commented prints

--- sam_student.py
+++ sam_student.py
@@ -4,8 +4,8 @@
 string_list = ['adfasklakr',
                 'sdkflafdal',
                 'dlkadjfakd']
-print('length of second strin: ', len(string_list[1]))
-print('what second seq really looks in interpreter: ', repr(string_list[1]))
+# print('length of second strin: ', len(string_list[1]))
+# print('what second seq really looks in interpreter: ', repr(string_list[1]))
 
 col_dic = {}
 
@@ -13,12 +13,12 @@ for i, c in enumerate(string_list[0]):
     col_list = []
     col_list.append(c)
     col_dic[i] = col_list
-print(col_dic)
+# print(col_dic)
 
 for i, s in enumerate(string_list):
     if i == 0:
         continue
-    print('i and s: ', i, s)
+    # print('i and s: ', i, s)
 
     for j, c in enumerate(s):
         # print('j: ', j)
@@ -29,7 +29,7 @@ print(col_dic)
 
 
 final_list = []
-for k, v in col_dic.items():
+for v in col_dic.values():
     freq_dic = {}
     # print(v)
     for l in v:
@@ -39,7 +39,7 @@ for k, v in col_dic.items():
         else:
             freq_dic[l] = 1
     final_list.append(freq_dic.items())
-    print("for {}th column, frequency is: {}".format(k, freq_dic))
+    # print("for {}th column, frequency is: {}".format(k, freq_dic))
 
 print(final_list)
 
